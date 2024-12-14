fcf2c7f0a2a5fbe9a190cd4a282619b445787542
seegyulater <marksy.petras7@gmail.com>
2024-12-14 23:26:58 +0800

    [feat] - fixed css styling on deployment

--- plm_archive_system/settings.py
+++ plm_archive_system/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'archive_app',
     'compressor',
+    'tailwind',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files settings for production
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
